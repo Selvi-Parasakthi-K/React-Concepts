c528d913d0f45b7a42f8f8d4d8ff779697dccbc6
Selvi-Parasakthi-K <kjparasakthi562003@gmail.com>
2026-03-25 13:47:10 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-2xl text-blue-900 flex justify-center items-center h-100">
+      Hello Selvi
+    </div>
+  );
+}
+
+export default App;
